771ab2dfa1c2b04ed57dca5450a9a5504ca1f2e0
Avinash Upadhyay <avinash.upadhyay@in.ey.com>
2023-12-21 14:39:08 +0530

    fix

--- src/main/java/com/avi/ArrayRotate.java
+++ src/main/java/com/avi/ArrayRotate.java
@@ -3,20 +3,22 @@ package com.avi;
 public class ArrayRotate {
 
     public static void main(String[] args) {
-        Integer[] arr = {1, 2, 3, 4, 5, 6};
+        int[] arr = {1, 2, 3, 4, 5, 6};
         int k = 2;
 
 //        rotateRight(arr, k);
-        rotateLeft(arr, k);
+//        rotateLeft(arr, k);
+        rotate(arr, k);
     }
 
-    public static void rotateRight(Integer[] arr, int k) {
+    public static void rotateRight(int[] arr, int k) {
 
-        int length = arr.length;
-        Integer[] temp = new Integer[length];
+        int l = arr.length;
+        int[] temp = new int[l];
         int j = 0;
+        k = k % l;
 
-        for (int i = k; i < length; i++) {
+        for (int i = k; i < l; i++) {
             temp[j] = arr[i];
             j++;
         }
@@ -24,31 +26,53 @@ public class ArrayRotate {
             temp[j] = arr[i];
             j++;
         }
-        System.arraycopy(temp, 0, arr, 0, length);
+        System.arraycopy(temp, 0, arr, 0, l);
 
         printTheArray(arr);
     }
 
-    public static void rotateLeft(Integer[] arr, int k) {
+    public static void rotateLeft(int[] arr, int k) {
 
-        int length = arr.length;
-        Integer[] temp = new Integer[length];
+        int l = arr.length;
+        int[] temp = new int[l];
         int j = 0;
+        k = k % l;
 
-        for (int i = length-k; i < length; i++) {
+        for (int i = l - k; i < l; i++) {
             temp[j] = arr[i];
             j++;
         }
-        for (int i = 0; i < length-k; i++) {
+        for (int i = 0; i < l - k; i++) {
             temp[j] = arr[i];
             j++;
         }
-        System.arraycopy(temp, 0, arr, 0, length);
+        System.arraycopy(temp, 0, arr, 0, l);
 
         printTheArray(arr);
     }
 
-    static void printTheArray(Integer[] arr) {
+    public static void rotate(int[] nums, int k) {
+
+        int l = nums.length;
+        k %= l;
+        reverse(nums, 0, l - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, l - 1);
+
+        printTheArray(nums);
+    }
+
+    public static void reverse(int[] a, int start, int end) {
+        while (start < end) {
+            int temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void printTheArray(int[] arr) {
         for (Integer i : arr) {
             System.out.print(i + " ");
         }
