36b1cb761db59a0d5ab6d43a55dd8be3a882fee7
Avinash Upadhyay <avinash.upadhyay@in.ey.com>
2024-03-02 00:23:19 +0530

    Recursion

---
+++ src/main/java/com/avi/dsa/Recursion.java
@@ -0,0 +1,59 @@
+package com.avi.dsa;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+//        func(1, 5);
+    }
+
+    static int factorial(int n) {
+        if (n <= 1) return n;
+        return n * factorial(n - 1);
+    }
+
+    /////// Print a list N times
+//    public static List<String> printNtimes(int n) {
+//        List<String> list = new ArrayList<>();
+//        addElement(list, n);
+//        return list;
+//    }
+//
+//    public static void addElement(List<String> list, int n) {
+//        if (list.size() == n) {
+//            return;
+//        }
+//        list.add("Coding Ninjas");
+//        addElement(list, n);
+//    }
+
+    /////// Print 1 to N
+//    static void func(int i, int n) {
+//        if (i > n) return;
+//        System.out.println(i);
+//        func(i + 1, n);
+//    }
+
+    /////// Print N to 1
+//    static void func(int i, int n){
+//        if(i<1) return;
+//        System.out.println(i);
+//        func(i-1,n);
+//    }
+
+//    public static int[] printNos(int x) {
+//        int [] arr = new int[x];
+//        recFunc(arr,x);
+//        return arr;
+//    }
+//
+//    static void recFunc(int[] arr,int x){
+//        if(x<=0){
+//            return;
+//        }
+//        arr[arr.length-x]=x;
+//        recFunc(arr, x-1);
+//    }
+
+
+}
--- src/main/java/com/avi/test.java
+++ src/main/java/com/avi/test.java
@@ -1,8 +1,6 @@
 package com.avi;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class test {
 
@@ -13,19 +11,19 @@ public class test {
         String s2 = "hello";
         String s3 = s1.intern();//returns string from pool, now it will be same as s2
         System.out.println(s1 == s2);//false because reference variables are pointing to different instance
-        System.out.println(s2 == s3);//true because reference variables are pointing to same instance*/
+        System.out.println(s2 == s3);//true because reference variables are pointing to same instance    */
 
+//        String x = """
+//                {"tranReceived":"N",
+//                "tranDate":"0001-01-01T00:00:00",
+//                "status":"99",
+//                "errorMsg":"TRAN DETAILS NOT AVAILABLE"}
+//                """;
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        JsonNode jsonNode = mapper.readTree(x);
+//        String dataValue = jsonNode.get("errorMsg").asText();
+//        System.out.println(dataValue);
 
-        String x = """
-                {"tranReceived":"N",
-                "tranDate":"0001-01-01T00:00:00",
-                "status":"99",
-                "errorMsg":"TRAN DETAILS NOT AVAILABLE"}
-                """;
-
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode jsonNode = mapper.readTree(x);
-        String dataValue = jsonNode.get("errorMsg").asText();
-        System.out.println(dataValue);
     }
 }
