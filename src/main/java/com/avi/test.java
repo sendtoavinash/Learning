fbca718759728109a9b786f726f7c887f16fda18
Avinash Upadhyay <sendtoavinash@outlook.com>
2024-05-05 22:59:56 +0530

    Update test.java

--- src/main/java/com/avi/test.java
+++ src/main/java/com/avi/test.java
@@ -5,25 +5,18 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 public class test {
 
     public static void main(String[] args) throws JsonProcessingException {
-//        System.out.println(new Date().getTime());
 
-        String s1 = new String("hello");
-        String s2 = "hello";
-        String s3 = s1.intern();//returns string from pool, now it will be same as s2
-        System.out.println(s1 == s2);//false because reference variables are pointing to different instance
-        System.out.println(s2 == s3);//true because reference variables are pointing to same instance
+        String x = """
+                {"tranReceived":"N",
+                "tranDate":"0001-01-01T00:00:00",
+                "status":"99",
+                "errorMsg":"TRAN DETAILS NOT AVAILABLE"}
+                """;
 
-//        String x = """
-//                {"tranReceived":"N",
-//                "tranDate":"0001-01-01T00:00:00",
-//                "status":"99",
-//                "errorMsg":"TRAN DETAILS NOT AVAILABLE"}
-//                """;
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode jsonNode = mapper.readTree(x);
-//        String dataValue = jsonNode.get("errorMsg").asText();
-//        System.out.println(dataValue);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(x);
+        String dataValue = jsonNode.get("errorMsg").asText();
+        System.out.println(dataValue);
 
     }
 }
