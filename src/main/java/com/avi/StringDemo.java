cd77a936317232ccd6b59f99afb4b55cd6225128
Avinash Upadhyay <sendtoavinash@outlook.com>
2024-05-05 23:00:53 +0530

    Update StringDemo.java

--- src/main/java/com/avi/StringDemo.java
+++ src/main/java/com/avi/StringDemo.java
@@ -24,6 +24,13 @@ public class StringDemo {
         System.out.println(isNullBlankString(test1));
         String test2 = "abc";
         System.out.println(isNullBlankString(test2));
+
+        String s1 = new String("hello");
+        String s2 = "hello";
+        String s3 = s1.intern();//returns string from pool, now it will be same as s2
+        System.out.println(s1 == s2);//false because reference variables are pointing to different instance
+        System.out.println(s2 == s3);//true because reference variables are pointing to same instance
+
     }
 
 //    public static String html2text(String html) {
