892aa312ed4be528dcb1e255155dbc3dcf75736a
Avinash Upadhyay <avinash.upadhyay@in.ey.com>
2024-05-09 20:44:10 +0530

    immutableDemo

--- src/main/java/com/avi/coreJava/ImmutableDemo.java
+++ src/main/java/com/avi/coreJava/ImmutableDemo.java
@@ -1,21 +1,84 @@
 package com.avi.coreJava;
 
-final class EmployeeImmutable {
-    final String pancardNumber;
+import java.util.HashMap;
+import java.util.Map;
 
-    public EmployeeImmutable(String pancardNumber) {
-        this.pancardNumber = pancardNumber;
+final class Student {
+
+    private final String name;
+    private final int regNo;
+    private final Map<String, String> metadata;
+
+    public Student(String name, int regNo, Map<String, String> metadata) {
+
+        this.name = name;
+        this.regNo = regNo;
+
+        // Creating Map object with reference to HashMap
+        // Declaring object of string type
+        Map<String, String> tempMap = new HashMap<>();
+
+        // Iterating using for-each loop
+        for (Map.Entry<String, String> entry : metadata.entrySet()) {
+            tempMap.put(entry.getKey(), entry.getValue());
+        }
+
+        this.metadata = tempMap;
     }
 
-    public String getPancardNumber() {
-        return pancardNumber;
+        public String getName() {
+        return name;
+    }
+
+    public int getRegNo() {
+        return regNo;
+    }
+
+    // Note that there should not be any setters
+
+    // User -defined type
+    // To get meta data
+    public Map<String, String> getMetadata() {
+
+        // Creating Map with HashMap reference
+        Map<String, String> tempMap = new HashMap<>();
+
+        for (Map.Entry<String, String> entry : this.metadata.entrySet()) {
+            tempMap.put(entry.getKey(), entry.getValue());
+        }
+        return tempMap;
     }
 }
 
 public class ImmutableDemo {
-    public static void main(String ar[]) {
-        EmployeeImmutable e = new EmployeeImmutable("ABC123");
-        String s1 = e.getPancardNumber();
-        System.out.println("Pancard Number: " + s1);
+
+    public static void main(String[] args) {
+
+        // Creating Map object with reference to HashMap
+        Map<String, String> map = new HashMap<>();
+
+        // Adding elements to Map object
+        // using put() method
+        map.put("1", "first");
+        map.put("2", "second");
+
+        Student s = new Student("ABC", 101, map);
+
+        // Calling the above methods 1,2,3 of class1
+        // inside main() method in class2 and
+        // executing the print statement over them
+        System.out.println(s.getName());
+        System.out.println(s.getRegNo());
+        System.out.println(s.getMetadata());
+
+        // Uncommenting below line causes error
+        // s.regNo = 102;
+
+        map.put("3", "third");
+        // Remains unchanged due to deep copy in constructor
+        System.out.println(s.getMetadata());
+        s.getMetadata().put("4", "fourth");
+        // Remains unchanged due to deep copy in getter
+        System.out.println(s.getMetadata());
     }
 }
