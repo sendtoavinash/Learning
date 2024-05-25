e13da6ef8185d8183d10b0c239ec5eac32f25d94
Avinash Upadhyay <sendtoavinash@outlook.com>
2024-05-25 15:33:51 +0530

    minor

--- src/main/java/com/avi/Main.java
+++ src/main/java/com/avi/Main.java
@@ -2,36 +2,8 @@ package com.avi;
 
 public class Main {
 
-    static int pair(String str) {
-        int i = 0;
-        int x = 0;
-        int y = 0;
-
-        String[] strArr = str.split("");
-
-        for (int j = 0; j < strArr.length / 2; j++) {
-            if (strArr[j].equals("{")) {
-                x++;
-            }
-        }
-        for (int k = strArr.length / 2; k < strArr.length; k++) {
-            if (strArr[k].equals("}")) {
-                y++;
-            }
-        }
-        i = Math.min(x, y);
-
-        return i;
-    }
-
     public static void main(String[] args) {
-//        {}=1
-//        {{}=1
-//        }{=0
-//            {{}}=2
-//            {{{}}}=3
-
-        System.out.println(pair("}{"));
 
     }
+
 }
