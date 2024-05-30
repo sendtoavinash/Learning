2fbe4d055103bcf75f1867ba56bb180fd16f26bf
Avinash Upadhyay <sendtoavinash@outlook.com>
2024-05-30 14:18:35 +0530

    minor

---
+++ src/main/java/com/avi/dsa/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+package com.avi.dsa;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length - 1];
+        int idx = 0;
+        while (idx < s1.length() && idx < s2.length()) {
+            if (s1.charAt(idx) == s2.charAt(idx)) {
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+
+    public static void main(String[] args) {
+
+        String[] ls = {"flower", "flow", "flight"};
+
+        System.out.println(longestCommonPrefix(ls));
+    }
+
+}
