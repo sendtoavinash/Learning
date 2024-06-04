ed08c11fcfc211060b91521d48c79cffbaaa3dc7
Avinash Upadhyay <avinash.upadhyay@in.ey.com>
2024-06-04 11:26:49 +0530

    minor

--- src/main/java/com/avi/dsa/Fibonacci.java
+++ src/main/java/com/avi/dsa/Fibonacci.java
@@ -4,26 +4,20 @@ import java.util.Arrays;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fib(5));
-        System.out.println(Arrays.toString(generateFibonacciNumbers(5)));
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(fib(i)+" ");
+        }
+
+//        System.out.println(Arrays.toString(generateFibonacciNumbers(5)));
     }
 
     /////// Recursion
-//    public static int fib(int n) {
-//        if(n<=1) return n;
-//        return fib(n-1)+fib(n-2);
-//    }
-
     public static int fib(int n) {
-        if (n <= 1) return n;
-        int[] f = new int[n + 1];
-        f[0] = 1;
-        f[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            f[i] = f[i - 1] + f[i - 2];
-        }
-        return f[n - 1];
+        if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
+
 
     public static int[] generateFibonacciNumbers(int n) {
         int[] f = new int[n];
--- src/main/java/com/avi/test.java
+++ src/main/java/com/avi/test.java
@@ -1,5 +1,6 @@
 package com.avi;
 
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
