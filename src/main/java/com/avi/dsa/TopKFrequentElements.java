2903f08d68047cf8a4925a21fb26702c7c12a33d
Avinash Upadhyay <avinash.upadhyay@in.ey.com>
2024-06-17 21:37:25 +0530

    minor

--- src/main/java/com/avi/dsa/TopKFrequentElements.java
+++ src/main/java/com/avi/dsa/TopKFrequentElements.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class topKFrequentElements {
+public class TopKFrequentElements {
 
     public static void main(String[] args) {
 
