5525c58c4c0b8595179dd63c5827f7da7934b7d7
Avinash Upadhyay <avinash.upadhyay@in.ey.com>
2024-06-26 11:29:42 +0530

    minor

---
+++ src/main/java/com/avi/Person.java
@@ -0,0 +1,13 @@
+package com.avi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+
+    private String name;
+
+
+}
---
+++ src/main/java/com/avi/sorting/Quick.java
@@ -0,0 +1,19 @@
+package com.avi.sorting;
+
+public class Quick {
+
+    public static void main(String[] args) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before selection sort:");
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+        sort(arr, n);
+    }
+
+    static void sort(int[] arr, int n) {
+
+    }
+}
